a02ad5d3b7131add83949a2d3ee8dd8594a1ce29
Mohd Amir <Amirkhan98375@gmail.com>
2024-01-26 01:12:03 +0530

    landing page start

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import { ScrollView, Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+const LandingPage= () =>{
+
+    return (
+      <View>
+        <Text>LandingPage</Text>
+      </View>
+    )
+  }
+
+
+export default LandingPage
